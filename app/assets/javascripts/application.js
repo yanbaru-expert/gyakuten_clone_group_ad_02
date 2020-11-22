4a77720b0668041df33b3bd7673e74525136f811
Toshiaki Kuramochi <toshi083073@gmail.com>
2020-11-22 11:48:27 +0900

    トップページにグラフ追加

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,12 @@
+//= require rails-ujs
+//= require activestorage
+// ***** 以下を追加 *****
+//= require Chart.min
+//= require flatpickr
+//= require flatpickr/l10n/ja
+//= require jquery3
+//= require popper
+//= require bootstrap-sprockets
+// ***** 以上を追加( turbolinks より上に追加) *****
+//= require turbolinks
+//= require_tree .
